c8e2a3092eee4dad03c31bfd40fb6ef371ca7c28
“jeferson” <jefersonsatoshi@hotmail.com>
2024-10-02 11:50:04 -0300

    Obtendo um usuario por id

--- src/main/java/com/jeferssonsatoshi/workshopmongo/services/UserService.java
+++ src/main/java/com/jeferssonsatoshi/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.jeferssonsatoshi.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jeferssonsatoshi.workshopmongo.domain.User;
 import com.jeferssonsatoshi.workshopmongo.repository.UserRepository;
+import com.jeferssonsatoshi.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
