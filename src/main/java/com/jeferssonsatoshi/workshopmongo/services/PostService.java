85dd7739277063687acc770405bcaac6090970e1
“jeferson” <jefersonsatoshi@hotmail.com>
2024-10-04 20:30:27 -0300

    Obtendo um post por id

---
+++ src/main/java/com/jeferssonsatoshi/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.jeferssonsatoshi.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jeferssonsatoshi.workshopmongo.domain.Post;
+import com.jeferssonsatoshi.workshopmongo.repository.PostRepository;
+import com.jeferssonsatoshi.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
